86e49c37b11b7a15190e8c51c09b33b8adcaee9e
AndreDewitt <72232545+AndreDewitt@users.noreply.github.com>
2020-10-01 18:33:59 -0500

    segundo commit

---
+++ contenido.php
@@ -0,0 +1,29 @@
+<?php
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/five.jpg"."||".
+				  "Mantícora"."||".
+				  "El oso de los five nights at freddys pero con cara rara.";
+
+		$datos[1]="img/claire.png"."||".
+				  "Dragon"."||".
+				  "Personaje popular de la franquicia Resident evil.";
+
+		$datos[2]="img/dom.jpg"."||".
+				  "Centauro"."||".
+				  "Iglesia.
+";
+
+		$datos[3]="img/gansito.jpg"."||".
+				  "Grifo"."||".
+				  "Un gansito vs el bajo precio de la gasolina.";
+
+		$datos[4]="img/house.jpg"."||".
+				  "Quimera"."||".
+				  "Una casa chida.";
+		return $datos;
+	}
+
+ ?>
