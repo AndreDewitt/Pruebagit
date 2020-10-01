238ccc2828630f1f850511833c47a8a137911569
AndreDewitt <72232545+AndreDewitt@users.noreply.github.com>
2020-10-01 17:43:24 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/*echo "Hola Mundo";
+
+echo $variable = "hola";
+$variable2 = 100;
+$variable3 = 2.2;
+$variable4 = false;
+
+if ($variable4) {
+  echo "Se cumplio";
+}
+
+if ($variable2 == 100) {
+  echo "ganaste";
+} else {
+  echo "Sigue participando";
+}
+$opcion = 1;
+
+switch ($opcion) {
+  case 1:
+    echo "suma";
+    break;
+
+    case 2:
+      echo "resta";
+      break;
+
+      case 3:
+        echo "division";
+        break;
+
+  default:
+  echo "opcion invalida";
+    break;
+}
+$contador = 0;
+while ($contador <= 5) {
+echo "sigue corriendo";
+}*/
+ ?>
